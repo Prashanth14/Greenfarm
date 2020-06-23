ea5780a9bf168c24946b35d16acd157fbd31fe6b
ManikumarGoud <mksgoud240397@gmail.com>
2020-06-23 13:08:00 +0530

    first commit

---
+++ server/validator/index.js
@@ -0,0 +1,24 @@
+exports.userSignupValidator = (req, res, next) => {
+  req.check("name", "Name is required").notEmpty();
+  req
+    .check("email", "Email must be between 3 to 32 characters")
+    .matches(/.+\@.+\..+/)
+    .withMessage("Email format is missing")
+    .isLength({
+      min: 4,
+      max: 32,
+    });
+  req.check("password", "Password is required").notEmpty();
+  req
+    .check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must contain at least 6 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number");
+  const errors = req.validationErrors();
+  if (errors) {
+    const firstError = errors.map((error) => error.msg)[0];
+    return res.status(400).json({ error: firstError });
+  }
+  next();
+};
