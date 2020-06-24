df1d6b1496f747887ac43763dd63272af25cfc42
Somagani Mani Kumar <39871789+ManikumarGoud@users.noreply.github.com>
2020-06-24 20:05:42 +0530

    Demo changes

--- client/src/core/About.js
+++ client/src/core/About.js
@@ -1,29 +1,35 @@
-import React, { Component } from 'react'
-import '../styles.css'
+import React, { Component } from "react";
+import "../styles.css";
 
 export class About extends Component {
-    render() {
-        return (
-            <div>
-                <div className="about-section">
-                    <div className="inner-width">
-                        <h1>About us</h1>
-                        <div className="border"></div>
-                        <div className="about-section-row">
-                            <div className="about-section-col">
-                                <div className="about">
-                                    <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sapien sit amet justo cursus vulputate. Suspendisse imperdiet aliquam urna quis rhoncus. Donec semper placerat efficitur. Sed non fermentum lorem. Vivamus accumsan lectus ac ex rhoncus, sit amet rhoncus dolor dapibus. Nam iaculis neque nunc, non elementum enim maximus non. Vestibulum rhoncus tincidunt enim sed maximus. Praesent euismod dolor eget nisi elementum, quis consequat mauris sagittis. Donec ligula augue, facilisis ut diam vitae, auctor cursus mauris. Maecenas pretium nec enim eget viverra. Etiam posuere pharetra auctor.
-                                    </p>
-                                    <a href="#a">Read More</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div>
+        <div className="about-section">
+          <div className="inner-width">
+            <h1>About us</h1>
+            <div className="border"></div>
+            <div className="about-section-row">
+              <div className="about-section-col">
+                <div className="about">
+                  <p>
+                    We are bridging the gap between the farmers and the society
+                    by providing a platform wherein farmers can contact the
+                    end-customer directly.
+                    <br /> We are committed to work for betterment of farmers
+                    and improve their economical condition via 'GreenFarm'.
+                    Also, End-constumer will be able to cut down his expenses by
+                    buying directly from farmers.
+                  </p>
+                  <a href="#a">Read More</a>
                 </div>
+              </div>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
--- client/src/core/Footer.js
+++ client/src/core/Footer.js
@@ -2,46 +2,64 @@ import React, { Component } from "react";
 
 import "../styles.css";
 
-import Logo from '../images/logo1.png'
+import Logo from "../images/logo1.png";
 
 export class Footer extends Component {
   render() {
     return (
       <div>
         <footer className="site-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-12 col-md-6">
-            <h6>About</h6>
-            <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et sapien sit amet justo cursus vulputate. Suspendisse imperdiet aliquam urna quis rhoncus. Donec semper placerat efficitur. Sed non fermentum lorem. Vivamus accumsan lectus ac ex rhoncus, sit amet rhoncus dolor dapibus. Nam iaculis neque nunc, non elementum enim maximus non.</p>
-          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p className="text-justify">
+                  We are bridging the gap between the farmers and the society by
+                  providing a platform wherein farmers can contact the
+                  end-customer directly and vice-versa. We are committed to work
+                  for betterment of farmers and improve their economical
+                  condition via 'Farmer Friend'. Also, End-constumer will be
+                  able to cut down his expenses by buying directly from farmers.
+                </p>
+              </div>
 
-          <div className="col-xs-6 col-md-3">
-            <img src={Logo} alt="nv" className="footer-logo" />
-          </div>
+              <div className="col-xs-6 col-md-3">
+                <img src={Logo} alt="nv" className="footer-logo" />
+              </div>
 
-          <div className="col-xs-6 col-md-3">
-            <h6>Quick Links</h6>
-            <ul className="footer-links">
-              <li><a href="#1">Home</a></li>
-              <li><a href="#1">Products</a></li>
-              <li><a href="#1">About Us</a></li>
-              <li><a href="#1">Log In</a></li>
-              <li><a href="#1">Register</a></li>
-            </ul>
+              <div className="col-xs-6 col-md-3">
+                <h6>Quick Links</h6>
+                <ul className="footer-links">
+                  <li>
+                    <a href="/">Home</a>
+                  </li>
+                  <li>
+                    <a href="/shop">Products</a>
+                  </li>
+                  <li>
+                    <a href="/">About Us</a>
+                  </li>
+                  <li>
+                    <a href="/signin">Log In</a>
+                  </li>
+                  <li>
+                    <a href="/signup">Register</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <hr />
           </div>
-        </div>
-        <hr/>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-sm-6 col-xs-12">
-            <p className="copyright-text">Copyright &copy; 2020 All Rights Reserved by Pegasus-2020.
-            </p>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-sm-6 col-xs-12">
+                <p className="copyright-text">
+                  Copyright &copy; 2020 All Rights Reserved by Pegasus-2020.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-</footer>
+        </footer>
       </div>
     );
   }
