ef664b31507c2346ca660a20ca3726acd8579db2
Somagani Mani Kumar <39871789+ManikumarGoud@users.noreply.github.com>
2020-06-25 10:22:05 +0530

    Admin to Farmer

--- client/src/user/AdminDashboard.js
+++ client/src/user/AdminDashboard.js
@@ -3,67 +3,67 @@ import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 
-import Navbar from '../core/Menu'
+import Navbar from "../core/Menu";
 
 const AdminDashboard = () => {
-    const {
-        user: { _id, name, email, role }
-    } = isAuthenticated();
+  const {
+    user: { _id, name, email, role },
+  } = isAuthenticated();
 
-    const adminLinks = () => {
-        return (
-            <div className="card">
-                <h4 className="card-header">Admin Links</h4>
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/create/category">
-                            Create Category
-                        </Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/create/product">
-                            Create Product
-                        </Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/admin/orders">
-                            View Orders
-                        </Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link className="nav-link" to="/admin/products">
-                            Manage Products
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        );
-    };
-
-    const adminInfo = () => {
-        return (
-            <div className="card mb-5">
-                <h3 className="card-header">User Information</h3>
-                <ul className="list-group">
-                    <li className="list-group-item">{name}</li>
-                    <li className="list-group-item">{email}</li>
-                    <li className="list-group-item">
-                        {role === 1 ? "Admin" : "Registered User"}
-                    </li>
-                </ul>
-            </div>
-        );
-    };
-
+  const adminLinks = () => {
     return (
-        <div>
-        <Navbar/>
-            <div className="row">
-                <div className="col-3">{adminLinks()}</div>
-                <div className="col-9">{adminInfo()}</div>
-            </div>
-        </div>
+      <div className="card">
+        <h4 className="card-header">Farmer Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/create/category">
+              Create Category
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/create/product">
+              Create Product
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/admin/orders">
+              View Orders
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/admin/products">
+              Manage Products
+            </Link>
+          </li>
+        </ul>
+      </div>
     );
+  };
+
+  const adminInfo = () => {
+    return (
+      <div className="card mb-5">
+        <h3 className="card-header">User Information</h3>
+        <ul className="list-group">
+          <li className="list-group-item">{name}</li>
+          <li className="list-group-item">{email}</li>
+          <li className="list-group-item">
+            {role === 1 ? "Farmer" : "Registered User"}
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className="row">
+        <div className="col-3">{adminLinks()}</div>
+        <div className="col-9">{adminInfo()}</div>
+      </div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
